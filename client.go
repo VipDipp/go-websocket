d169e337889ca9e21f8329b7efaa07d7c9c6dc2a
VipDipp <kuxaruk03@ukr.net>
2022-05-26 16:32:37 +0300

    cleaning

--- client.go
+++ client.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
-	"github.com/streadway/amqp"
 )
 
 const (
@@ -130,48 +129,6 @@ func (c *Client) writePump() {
 	}
 }
 
-func (c *Client) receive() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	var forever chan struct{}
-
-	go func() {
-		for d := range msgs {
-			c.hub.broadcast <- d.Body
-		}
-	}()
-
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
-}
-
 func (c *Client) Run() {
 	pool := make(chan []byte, 64)
 	i := 1
@@ -206,7 +163,6 @@ func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	// Allow collection of memory referenced by the caller by doing all work in
 	// new goroutines.
 	go client.Run()
-	go client.receive()
 	go client.writePump()
 	//go client.readPump()
 }
